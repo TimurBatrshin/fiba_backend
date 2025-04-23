cb4d732b61fca70717db9b659fe2894fb52309c9
TimurBatrshin <65765228+TimurBatrshin@users.noreply.github.com>
2025-04-23 16:52:00 +0300

    fix front

--- src/main/java/com/fiba/api/config/CorsConfig.java
+++ src/main/java/com/fiba/api/config/CorsConfig.java
@@ -15,9 +15,6 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                    "https://timurbatrshin-fiba-backend-0722.twc1.net",
-                    "https://timurbatrshin-fiba-backend-1aa7.twc1.net", 
-                    "https://timurbatrshin-fiba-backend-95ba.twc1.net", 
                     "http://localhost:8099", 
                     "https://dev.bro-js.ru"
                 )
@@ -32,9 +29,6 @@ public class CorsConfig implements WebMvcConfigurer {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         
-        config.addAllowedOrigin("https://timurbatrshin-fiba-backend-0722.twc1.net");
-        config.addAllowedOrigin("https://timurbatrshin-fiba-backend-1aa7.twc1.net");
-        config.addAllowedOrigin("https://timurbatrshin-fiba-backend-95ba.twc1.net");
         config.addAllowedOrigin("http://localhost:8099");
         config.addAllowedOrigin("https://dev.bro-js.ru");
         config.addAllowedHeader("*");
