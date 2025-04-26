e9a816ee6192158292d2430166cf2b89ba149b3f
TimurBatrshin <batrshintimur@yandex.ru>
2025-04-26 10:02:19 +0300

    fix20

--- src/main/java/com/fiba/api/config/WebConfig.java
+++ src/main/java/com/fiba/api/config/WebConfig.java
@@ -10,7 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.PathResourceResolver;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 import java.io.IOException;
 
 /**
